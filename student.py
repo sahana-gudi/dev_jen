07d1378e4370a3550a720ebad95ac7d0e97f4659
sahana-gudi <sonugudi472@gmail.com>
2025-11-22 09:29:35 +0530

    Update and rename README.md to student.py

---
+++ student.py
@@ -0,0 +1,11 @@
+# dev_jen
+import sys
+if len(sys.argv) !=3:
+print("Usage: python student.py <name><rollno>")
+sys.exit(1)
+script_name = sys.argv[0]
+name = sys.argv[1]
+rollno = sys.argv[2]
+print("Script Name:", script_name)
+print("Student Name:", name)
+print("Roll Number:", rollno)
